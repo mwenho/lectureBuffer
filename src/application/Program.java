cea42bde561c1a38dcaecbe9c93821d899924581
Joao Dumba <mwenhondumba@gamil.com>
2021-02-09 12:43:26 -0800

    first commit2

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,17 +9,10 @@ public class Program {
     public static void main(String[] args) {
 
         String path = "C:\\Users\\yavam\\Documents\\lecture\\in.txt";
-<<<<<<< HEAD
+
 
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-=======
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
->>>>>>> origin/main
 
             String line = br.readLine();
 
@@ -31,22 +24,7 @@ public class Program {
         catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
-<<<<<<< HEAD
 
-=======
-        finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
->>>>>>> origin/main
+
     }
 }
